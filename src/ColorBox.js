ff2bcc257e079bed4cfd1f4ace2d29a6eddbe515
David Valencia <davidvalenciac9@gmail.com>
2020-05-01 19:06:22 -0400

    adding copy clipboard functionality and transitions and more css

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,50 @@
 import React, {Component} from 'react';
 import './ColorBox.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export class ColorBox extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      copied: false,
+    };
+    this.changeCopyState = this.changeCopyState.bind (this);
+  }
+
+  changeCopyState () {
+    this.setState (
+      {
+        copied: true,
+      },
+      () => {
+        setTimeout (() => this.setState ({copied: false}), 1500);
+      }
+    );
+  }
+
   render () {
     const {name, background} = this.props;
+    const {copied} = this.state;
     return (
-      <div style={{background}} className="ColorBox">
-        <div className="copy-container">
-          <div className="box-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{background}} className="ColorBox">
+          <div
+            style={{background}}
+            className={`copy-overlay ${copied && 'show'} `}
+          />
+          <div className={`copy-msg ${copied && 'show'}`}>
+            <h1>copied!</h1>
+            <p>{background}</p>
           </div>
-          <button className="copy-button">COPY</button>
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">COPY</button>
+          </div>
+          <span className="see-more">MORE</span>
         </div>
-        <span className="see-more">MORE</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
