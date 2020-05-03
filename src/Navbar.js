69b07d466a8b245ab6b7ff8e33e9f89b05dc254a
David Valencia <davidvalenciac9@gmail.com>
2020-05-03 13:56:42 -0400

    add dropdown menu to chose between hex rgba and rgba with material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,30 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
+/*** MATERIAL UI IMPORTS ***/
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 export class Navbar extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      format: 'hex',
+    };
+    this.handleChange = this.handleChange.bind (this);
+  }
+
+  handleChange (e) {
+    this.setState ({
+      format: e.target.value,
+    });
+    this.props.handleChange (e.target.value);
+  }
+
   render () {
     const {level, changeLevel} = this.props;
+    const {format} = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -37,7 +58,13 @@ export class Navbar extends Component {
             />
           </div>
         </div>
-
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #FFFFFF</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
       </header>
     );
   }
