1377d3d48ac7f3f51199656826bd6b8551d26369
David Valencia <davidvalenciac9@gmail.com>
2020-05-04 16:10:03 -0400

    Adding dynamic text color for lighter or darker text with chroma

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './ColorBox.css';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import {Link} from 'react-router-dom';
+import chroma from 'chroma-js';
 
 export class ColorBox extends Component {
   constructor (props) {
@@ -26,6 +27,8 @@ export class ColorBox extends Component {
   render () {
     const {name, background, id, paletteId, showLink} = this.props;
     const {copied} = this.state;
+    const isDarkColor = chroma (background).luminance () <= 0.08;
+    const isLightColor = chroma (background).luminance () >= 0.50;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{background}} className="ColorBox">
@@ -35,20 +38,28 @@ export class ColorBox extends Component {
           />
           <div className={`copy-msg ${copied && 'show'}`}>
             <h1>copied!</h1>
-            <p>{background}</p>
+            <p className={`${isLightColor && 'dark-text'}`}>
+              {background}
+            </p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span>{name}</span>
+              <span className={isDarkColor && 'light-text'}>{name}</span>
             </div>
-            <button className="copy-button">COPY</button>
+            <button className={`copy-button ${isLightColor && 'dark-text'}`}>
+              COPY
+            </button>
           </div>
           {showLink &&
             <Link
               to={`/palette/${paletteId}/${id}`}
               onClick={e => e.stopPropagation ()}
             >
-              {' '}<span className="see-more">MORE</span>{' '}
+              {' '}
+              <span className={`see-more ${isLightColor && 'dark-text'}`}>
+                MORE
+              </span>
+              {' '}
             </Link>}
         </div>
       </CopyToClipboard>
